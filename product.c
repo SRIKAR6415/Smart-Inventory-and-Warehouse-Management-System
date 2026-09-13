9cd4e2e6c6d671486b7593afb43e03c4b4a32099
SRIKAR <srinivassrikar39@gmail.com>
2026-09-13 13:18:44 +0530

    Added updateProduct function

--- product.c
+++ product.c
@@ -55,6 +55,50 @@ void displayProducts(struct Product p[], int n)
         printf("Supplier Id: %d\n",p[i].supplierId);
     }
 }
+void updateProduct(struct Product p[], int n)
+{
+    int id;
+    int i;
+    int found = 0;
+
+    printf("Enter product id to update: ");
+    scanf("%d",&id);
+
+    for(i=0;i<n;i++)
+    {
+        if(p[i].id == id)
+        {
+            printf("Enter new product name: ");
+            scanf("%s",p[i].name);
+
+            printf("Enter new category: ");
+            scanf("%s",p[i].category);
+
+            printf("Enter new price: ");
+            scanf("%f",&p[i].price);
+
+            printf("Enter new stock: ");
+            scanf("%d",&p[i].stock);
+
+            printf("Enter new minimum stock: ");
+            scanf("%d",&p[i].minStock);
+
+            printf("Enter new supplier id: ");
+            scanf("%d",&p[i].supplierId);
+
+            found = 1;
+
+            printf("Product updated successfully\n");
+
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("Product not found\n");
+    }
+}
 
 int main()
 {
@@ -69,17 +113,8 @@ int main()
         addProduct(p,i);
     }
     displayProducts(p,n);
-    for(i=0;i<n;i++)
-    {
-        printf("\nProduct %d\n",i+1);
-        printf("Id: %d\n",p[i].id);
-        printf("Name: %s\n",p[i].name);
-        printf("Category: %s\n",p[i].category);
-        printf("Price: %.2f\n",p[i].price);
-        printf("Stock: %d\n",p[i].stock);
-        printf("Minimum Stock: %d\n",p[i].minStock);
-        printf("Supplier Id: %d\n",p[i].supplierId);
-    }
+    updateProduct(p,n);
+    displayProducts(p,n);
 
     return 0;
 }
